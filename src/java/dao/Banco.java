aad13ea6ad8dfd04a2afe94777a59d392abc34d9
estefaneluz <estefanecortes@gmail.com>
2021-10-23 21:49:08 -0300

    criando usuario admin no banco e função se usuario existe

--- src/java/dao/Banco.java
+++ src/java/dao/Banco.java
@@ -1,10 +1,32 @@
 
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import modelo.Usuario;
+
 /**
  *
  * @author Estef
  */
 public class Banco {
+    private static List<Usuario> listaUsuarios = new ArrayList<>();
     
+    static {
+        Usuario usuario = new Usuario();
+        usuario.setLogin("admin");
+        usuario.setSenha("123");
+        
+        listaUsuarios.add(usuario);
+    }
+    
+    public Usuario existeUsuario(String login, String senha){
+        for(Usuario usuario : listaUsuarios) {
+            if(usuario.validar(login, senha)){
+                return usuario;
+            }
+        }
+        
+        return null;
+    }
 }
